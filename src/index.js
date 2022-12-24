68b60006be6b90983cf47e89b2954bfe60a59bec
kellyg418 <121138635+kellyg418@users.noreply.github.com>
2022-12-23 23:48:17 -0800

    Created the index.html

---
+++ src/index.js
@@ -0,0 +1,44 @@
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let dayIndex = currentTime.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+dateElement.innerHTML = `${days[dayIndex]} ${hours}:${minutes}`;
+
+function showWeatherConditions(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "a7498752755deb8494c61ba3d2aba42d";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showWeatherConditions);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
