243036ac91132f086316dfb597ac1dd14342797c
kellyg418 <121138635+kellyg418@users.noreply.github.com>
2023-01-13 00:00:32 -0800

    removed image folder, updated background and headings color,

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "a7498752755deb8494c61ba3d2aba42d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
